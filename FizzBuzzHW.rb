f2ee4c2c65b0d0e5d200e53422647c47bc73d4db
aljarrell <alan.k.jarrell@gmail.com>
2018-09-25 09:20:20 -0400

    FizzBuzzHW

---
+++ FizzBuzzHW.rb
@@ -0,0 +1,18 @@
+#Fizz Buzz Homework
+
+count = 1
+
+while count < 51 do
+  if count % 5 == 0 && count % 10 == 0
+    puts "ford mustang"
+  elsif
+    count % 5 == 0
+    puts "ford"
+  elsif
+    count % 6 == 0
+    puts "is the best"
+  else
+  puts count
+  end
+  count = count + 1
+end
